35f0b9ea4158081ec0d534c49b32d2b32326bb78
风中的消息 <oncwnuFNUqzIgXL7S5yufx6pkzW4@git.weixin.qq.com>
2021-01-27 07:48:21 +0800

    add

--- src/20output/webpack.config.js
+++ src/20output/webpack.config.js
@@ -7,8 +7,15 @@ module.exports = {
     output: {
         // 文件名称（指定名称+目录）
         filename: '[name].js',
-        // 输出文件目录（奖励啊）
-        path: resolve(__dirname, "build")
+        // 输出文件目录（将来左右资源输出的公共目录）
+        path: resolve(__dirname, "build"),
+        // 所有资源引入的公共路径 --> path的前面（一般用于生产环境）
+        publicPath: '/',
+        chunkFilename: 'js/[name]_chunk.js', // 非入口chunk的名称
+        //library: '[name]', // 全局整个库向外暴漏的变量名
+        //libraryTarget: 'window', // 变量名添加到哪个上 浏览器
+        //libraryTarget: 'global', // 变量名添加到哪个上 node
+        //libraryTarget: 'commonjs', // 变量名添加到哪个上 
     },
     plugins: [
         new HtmlWebpackPlugin()
